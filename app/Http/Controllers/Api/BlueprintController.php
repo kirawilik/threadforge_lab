4363e04000a41e554243177cfbdc80850661df39
kirawilik <khadijakchini@gmail.com>
2026-07-03 14:33:50 +0100

    add controller avec store update

--- app/Http/Controllers/Api/BlueprintController.php
+++ app/Http/Controllers/Api/BlueprintController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Blueprint;
+use App\Http\Requests\UpdateBlueprintRequest;
+use App\Http\Requests\StoreBlueprintRequest;
+use App\Http\Resources\BlueprintResource;
 
 class BlueprintController extends Controller
 {
@@ -12,17 +16,23 @@ class BlueprintController extends Controller
      */
     public function index()
     {
-         $blueprints = auth()->user()->blueprints()->latest()->get();
-
+return BlueprintResource::collection(auth()->user()->blueprints()->latest()->get());
         return response()->json($blueprints, 200);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreBlueprintRequest $request)
     {
-        //
+       $blueprint = auth()->user()->blueprints()->create(
+        $request->validated()
+    );
+
+
+        return (new BlueprintResource($blueprint))
+            ->response()
+            ->setStatusCode(201);
     }
 
     /**
@@ -30,22 +40,30 @@ class BlueprintController extends Controller
      */
     public function show(string $id)
     {
-         return response()->json($blueprint, 200);
+        $blueprint = Blueprint::findOrFail($id);
+
+    return response()->json($blueprint, 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateBlueprintRequest $request, Blueprint $blueprint)
     {
-        //
+       $blueprint->update($request->validated());
+
+        return new BlueprintResource($blueprint);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+   public function destroy(Blueprint $blueprint)
     {
-        //
+        $blueprint->delete();
+
+        return response()->json([
+            'message' => 'blueprint deleted successfully'
+        ], 200);
     }
 }
