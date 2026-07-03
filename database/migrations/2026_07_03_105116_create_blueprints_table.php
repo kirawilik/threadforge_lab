48d2d018a38e5b38c4ffc2eede976131853c8c9a
kirawilik <khadijakchini@gmail.com>
2026-07-03 12:41:51 +0100

    add migration blueprint

---
+++ database/migrations/2026_07_03_105116_create_blueprints_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blueprints', function (Blueprint $table) {
+            $table->id();
+             $table->foreignId('user_id')
+        ->constrained()
+        ->cascadeOnDelete();
+            $table->string('name');
+    $table->string('target_audience');
+    $table->string('tone');
+    $table->integer('max_characters')->default(250);
+    $table->integer('max_hashtags')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blueprints');
+    }
+};
