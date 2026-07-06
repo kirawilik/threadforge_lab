34e40b69f4a845c1561dcb13ad3dc2f101d70954
kirawilik <khadijakchini@gmail.com>
2026-07-06 11:01:48 +0100

    add post resource

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+     
+        return [
+
+            'id' => $this->id,
+
+            'hook_propose' => $this->hook_propose,
+
+            'body_points' => $this->body_points,
+
+            'technical_readability_score'
+                => $this->technical_readability_score,
+
+            'suggested_hashtags'
+                => $this->suggested_hashtags,
+
+            'tone_compliance_justification'
+                => $this->tone_compliance_justification,
+
+            'status' => $this->status,
+
+            'created_at' => $this->created_at,
+        ];
+    }
+}
